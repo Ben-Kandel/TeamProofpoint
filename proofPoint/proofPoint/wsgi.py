b33c2d00b7862c10ffb4216944d5b4b963576fcc
southw41 <southw41@msu.edu>
2020-09-18 19:20:21 -0400

    framework

---
+++ proofPoint/proofPoint/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for proofPoint project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proofPoint.settings')
+
+application = get_wsgi_application()
