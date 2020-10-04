a67453142ed62f45ec6d63d055c9bff1386f9f6d
Ben-Kandel <deltaforce126@gmail.com>
2020-10-04 12:42:23 -0400

    added missing files

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,10 @@
 //components
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+//import { BrowserRouter, Route, Switch } from "react-router-dom";
 import InfoCard from "./InfoCard";
 import InfoButton from "./InfoButton";
-import CustomChart from "./CustomChart";
+import {CustomChart} from "./CustomChart";
 
 //css stuff
 import './index.css';
@@ -29,10 +30,40 @@ ReactDOM.render(
       <InfoButton text="Go Back"/>
     </div>
     <div class="item5">
-      <CustomChart width="400px" height="300px"/>
+      <CustomChart></CustomChart>
     </div>
 
   </div>
   ,
   document.getElementById('root')
 );
+
+/*
+import Home from "./components/Home";
+import About from "./components/About";
+import Navigation from './components/Navigation';
+
+class App extends Component{
+  render(){
+    return(
+      <BrowserRouter>
+        <div>
+          <Navigation>
+            <Switch>
+              <Route path="/" component={Home} exact/>
+              <Route path="/about" component={About}/>
+            </Switch>
+          </Navigation>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <p1>Fuck!</p1>
+  </div>,
+  document.getElementById("root")
+)
+*/
