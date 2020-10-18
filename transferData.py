17954d4f4fb60f90d06a6f102407a72426cf762a
southw41 <southw41@msu.edu>
2020-10-18 16:39:13 -0400

    1.3 better loading+new DB

--- transferData.py
+++ transferData.py
@@ -9,9 +9,11 @@
 import sqlite3
 import pandas as pd
 
-def transfer():
-    conn = sqlite3.connect("proofpoint_mockdata.db")
-    data = pd.read_sql_query("select * from Testing", conn)
+def transfer(nameDB, copyTable):
+    statisticsNameDB = "statistics_" + nameDB
+
+    conn = sqlite3.connect("proofpointDB.db")
+    data = pd.read_sql_query(f"select * from {copyTable}", conn)
 
     e_amount = 0
     positive= 0
@@ -20,7 +22,7 @@ def transfer():
     for i in data['subject']:
         if i == "Election":
             e_amount +=1
-    for i in data["P/N"]:
+    for i in data["pn"]:
         if i == "Positive":
             positive +=1
         elif i == "Negative":
@@ -33,11 +35,11 @@ def transfer():
         : [e_amount]}
     stats = pd.DataFrame(data = d)
     conn2 = sqlite3.connect("db.sqlite3")
-
-    data.to_sql('leads_lead', con=conn2, if_exists = "replace")
-    stats.to_sql('statistics', con=conn2, if_exists = "replace")
-    data2= pd.read_sql_query("select * from leads_lead", conn2)
-    test =  pd.read_sql_query("select * from statistics", conn2)
+    data.index.name = "id"
+    data.to_sql(nameDB, con=conn2, if_exists = "replace")
+    stats.to_sql(statisticsNameDB, con=conn2, if_exists = "replace")
+    data2= pd.read_sql_query(f"select * from {nameDB}", conn2)
+    test =  pd.read_sql_query(f"select * from {statisticsNameDB}", conn2)
     conn.close()
     conn2.close()
 print("Test")
