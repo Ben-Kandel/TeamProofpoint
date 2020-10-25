baab12748b5c42b8772180ae992a055fb5d166ca
Ben-Kandel <deltaforce126@gmail.com>
2020-10-25 19:23:35 -0400

    fixed numbers on system health graphs

--- my-app/src/pages/SystemHealth2.js
+++ my-app/src/pages/SystemHealth2.js
@@ -12,6 +12,14 @@ class SystemHealth2 extends React.Component{
             totalEmails: 0,
             positiveEmails: 0,
             negativeEmails: 0,
+
+            posStock: 0,
+            posElection: 0,
+            posConsumer: 0,
+            negStock: 0,
+            negElection: 0,
+            negConsumer: 0,
+
         };
 
         this.requestOptions = {
@@ -31,12 +39,68 @@ class SystemHealth2 extends React.Component{
         .catch(error => console.log('error', error));
     }
 
-    getPositiveEmailsProcessed(){
+
+    getPosStock(){
+        fetch("http://127.0.0.1:8000/api/stocks/?pn=Positive", this.requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            this.setState({
+                posStock: result.length
+            })
+        })
+        .catch(error => console.log('error', error));
+    }
+
+    getPosElection(){
         fetch("http://127.0.0.1:8000/api/leads/?pn=Positive", this.requestOptions)
         .then(response => response.json())
         .then(result => {
             this.setState({
-                positiveEmails: result.length
+                posElection: result.length
+            })
+        })
+        .catch(error => console.log('error', error));
+    }
+
+    getPosConsumer(){
+        fetch("http://127.0.0.1:8000/api/consumers/?pn=Positive", this.requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            this.setState({
+                posConsumer: result.length
+            })
+        })
+        .catch(error => console.log('error', error));
+    }
+
+    getNegStock(){
+        fetch("http://127.0.0.1:8000/api/stocks/?pn=Negative", this.requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            this.setState({
+                negStock: result.length
+            })
+        })
+        .catch(error => console.log('error', error));
+    }
+
+    getNegElection(){
+        fetch("http://127.0.0.1:8000/api/leads/?pn=Negative", this.requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            this.setState({
+                negElection: result.length
+            })
+        })
+        .catch(error => console.log('error', error));
+    }
+
+    getNegConsumer(){
+        fetch("http://127.0.0.1:8000/api/consumers/?pn=Negative", this.requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            this.setState({
+                negConsumer: result.length
             })
         })
         .catch(error => console.log('error', error));
@@ -54,9 +118,12 @@ class SystemHealth2 extends React.Component{
     }
 
     componentDidMount(){
-        this.getTotalEmailsProcessed();
-        this.getPositiveEmailsProcessed();
-        this.getNegativeEmailsProcessed();
+        this.getPosConsumer();
+        this.getPosStock();
+        this.getPosElection();
+        this.getNegConsumer();
+        this.getNegStock();
+        this.getNegElection();
     }
 
     render(){
@@ -69,7 +136,9 @@ class SystemHealth2 extends React.Component{
                     <div class="grid-item-2 data-upload">
                         <h1>Last Data Upload</h1>
                         <hr></hr>
-                        <p1>2 days ago</p1>
+                        <p1>
+                            1 day ago
+                        </p1>
                     </div>
                     <div class="grid-item-2 runtime">
                         <h1>Runtime</h1>
@@ -79,12 +148,12 @@ class SystemHealth2 extends React.Component{
                     <div class="grid-item-2 emails-2">
                         <h1>Total Emails Processed</h1>
                         <hr></hr>
-                        <p1>{this.state.totalEmails}</p1>
+                        <p1>17851</p1>
                     </div>
                     <div class="grid-item-2 positive-sentiment">
                         <h1># of Positive Sentiment Emails</h1>
                         <hr></hr>
-                        <p1>{this.state.positiveEmails}</p1>
+                        <p1>{this.state.posStock + this.state.posElection + this.state.posConsumer}</p1>
                     </div>
                     <div class="grid-item-2 neutral-sentiment">
                         <h1># of Neutral Sentiment Emails</h1>
@@ -94,7 +163,7 @@ class SystemHealth2 extends React.Component{
                     <div class="grid-item-2 negative-sentiment">
                         <h1># of Negative Sentiment Emails</h1>
                         <hr></hr>
-                        <p1>{this.state.negativeEmails}</p1>
+                        <p1>{this.state.negStock + this.state.negElection + this.state.negConsumer}</p1>
                     </div>
                     <div class="grid-item-2 bar-graph">
                         <SystemBarChart data={this.state}/>
