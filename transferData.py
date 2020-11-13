b1bbabc6ec3198eb79150f653322e64fd94b557c
xumatthew <xu.matthew01@gmail.com>
2020-11-13 15:05:16 -0500

    Added stocks to django database

--- transferData.py
+++ transferData.py
@@ -45,18 +45,27 @@ def transfer(nameDB, copyTable):
     data.to_sql(nameDB, con=conn2, if_exists = "replace")
     stats.to_sql(statisticsNameDB, con=conn2, if_exists = "replace")
 
-    #------------------------------------------------------Index Funds
+    #------------------------------------------------------Index Funds and Stock Prices
 
     sp = pd.read_csv("^GSPC.csv")
     dow = pd.read_csv("^DJI.csv")
     nasdaq = pd.read_csv("^IXIC.csv")
-
+    aapl = pd.read_csv("AAPL.csv")
+    amzn = pd.read_csv("AMZN.csv")
+    goog = pd.read_csv("GOOG.csv")
+    tsla = pd.read_csv("TSLA.csv")
+    msft = pd.read_csv("MSFT.csv")
     master = pd.DataFrame(columns = ["date", "price", "name"])
 
     for d, i in enumerate(dow['Date']):
         master.loc[len(master.index)] = [dow['Date'][d], dow["Adj Close"][d], "DOW"]
         master.loc[len(master.index)] = [sp['Date'][d], sp["Adj Close"][d], "S&P500"]
         master.loc[len(master.index)] = [nasdaq['Date'][d], nasdaq["Adj Close"][d], "NASDAQ"]
+        master.loc[len(master.index)] = [aapl['Date'][d], aapl["Adj Close"][d], "AAPL"]
+        master.loc[len(master.index)] = [amzn['Date'][d], amzn["Adj Close"][d], "AMZN"]
+        master.loc[len(master.index)] = [msft['Date'][d], msft["Adj Close"][d], "MSFT"]
+        master.loc[len(master.index)] = [tsla['Date'][d], tsla["Adj Close"][d], "TSLA"]
+        master.loc[len(master.index)] = [goog['Date'][d], goog["Adj Close"][d], "GOOG"]
     master.index.name = "id"
     master.to_sql("prices_prices", con =conn2, if_exists = "replace")
 
