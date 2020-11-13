986e3682bf447137df05f0ac05fb6c52e773e55b
Ben-Kandel <deltaforce126@gmail.com>
2020-11-13 17:14:14 -0500

    more graphs!!

--- my-app/src/components/IndivStockChart.js
+++ my-app/src/components/IndivStockChart.js
@@ -1,7 +1,7 @@
 import { Line } from "react-chartjs-2";
 import React from "react";
 
-export default function IndivStockChart(props){
+export function IndivStockEmailsChart(props){
 
     let fixData = (data) => {
         return data.map(element => {
@@ -9,14 +9,9 @@ export default function IndivStockChart(props){
         });
     };
 
-    // console.log('im indiv and I received this in my props:');
-    // console.log(props.data);
-
     const data = {
         datasets: [
             {
-                label: 'Test',
-                //data: fixData([new Date(2020, 9, 2), 3348.419922,]), //props.temp
                 data: fixData(props.data),
                 fill: false,
                 backgroundColor: 'rgba(0, 200, 0)',
@@ -30,7 +25,19 @@ export default function IndivStockChart(props){
         maintainAspectRatio: false,
         title: {
             display: true,
-            text: 'Index Closing Values per Day',
+            text: 'Relevant Emails Received per Day',
+        },
+        legend: {
+            display: false,
+        },
+        tooltips: {
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    let number = tooltipItem.yLabel;
+                    let label = `${number} emails received`;
+                    return label;
+                }
+            },
         },
         scales: {
             xAxes: [{
@@ -54,7 +61,73 @@ export default function IndivStockChart(props){
     };
 
     return(
-        <div id="index-prices-chart-container">
+        <div id="individual-stock-emails-chart-container">
+            <Line data={data} options={options}></Line>
+        </div>
+    )
+};
+
+export function IndivStockPricesChart(props){
+
+    let fixData = (data) => {
+        return data.map(element => {
+            return {x: element.date, y: element.price}
+        });
+    }
+
+    const data = {
+        datasets: [
+            {
+                label: 'Hello',
+                data: fixData(props.data),
+                fill: false,
+                backgroundColor: 'rgba(0, 200, 0)',
+                borderColor: 'rgba(0, 200, 0)',
+            },
+        ],
+    };
+
+    const options = {
+        maintainAspectRatio: false,
+        title: {
+            display: true,
+            text: 'Stock Prices per Day',
+        },
+        legend: {
+            display: false,
+        },
+        tooltips: {
+            callbacks: {
+                label: function(tooltipItem, data) {
+                    let roundedPrice = tooltipItem.yLabel.toFixed(2);
+                    let label = `$${roundedPrice}`;
+                    return label;
+                }
+            },
+        },
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    unit: 'day',
+                    displayFormats: {
+                        day: 'MMM D',
+                    }
+                },
+                ticks: {
+                    beginAtZero: true,
+                }
+            }],
+            yAxes: [{
+                ticks: {
+                    beginAtZero: false,
+                }
+            }],
+        }
+    };
+
+    return(
+        <div id="individual-stock-prices-chart-container">
             <Line data={data} options={options}></Line>
         </div>
     )
