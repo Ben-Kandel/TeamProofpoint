7c446246a90b5900822d424bb52f62adec27e759
Ben-Kandel <deltaforce126@gmail.com>
2020-11-15 12:50:28 -0500

    removed unused routes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,9 +13,6 @@ import SystemHealth2 from "./pages/SystemHealth2";
 import Home from "./pages/Home"; 
 import Election from "./pages/Election";
 import Stocks from "./pages/Stocks.js";
-import Results2 from "./pages/Results2.js";
-
-
 
 export default function App() {
 
@@ -26,9 +23,6 @@ export default function App() {
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
-          <Route path="/about" >
-            <About />
-          </Route>
           <Route path="/health" >
             <SystemHealth />
           </Route>
@@ -41,9 +35,6 @@ export default function App() {
           <Route path = "/stocks" >
             <Stocks/>
           </Route>
-          <Route path = "/results2" >
-            <Results2/>
-          </Route>
           <Route path="/" >
             <Home/>
           </Route>
@@ -52,11 +43,6 @@ export default function App() {
     </BrowserRouter>
 
   );
-}
-
-//this is just temporary, this will be a class in the pages directory.
-function About() {
-  return <h2>this is the about page...</h2>;
 }
 
 ReactDOM.render(
